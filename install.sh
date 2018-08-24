16814c1fe2a167b3e664cfbafd5bc88e392df03f
Dino <dino.srdoc@degordian.com>
2018-08-24 11:33:34 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -d ~/Library/Developer/Xcode/Templates ]; then
+    echo "Templates directory doesn't exist. Creating one" 
+    mkdir ~/Library/Developer/Xcode/Templates
+fi
+
 ln -s $PWD/templates/ ~/Library/Developer/Xcode/Templates/
 echo "Install complete. Please restart XCode (if applicable)."
